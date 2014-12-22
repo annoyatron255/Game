60e026391e99f3673a119077e7856495929ea8ff
annoyatron255 <none>
2014-12-22 14:41:47 -0600

    Updated Everything
    Fixed and now using textures for gfx. split files apart, updated
    graphics

---
+++ general.h
@@ -0,0 +1,2 @@
+int overlap(SDL_Rect rect1, SDL_Rect rect2);
+int touching(SDL_Rect rect1, SDL_Rect rect2);
---
+++ input.h
@@ -0,0 +1,10 @@
+int inputLoop();
+
+extern int speed;
+extern int wait;
+extern bool done;
+extern bool shift;
+extern bool up;
+extern bool down;
+extern bool left;
+extern bool right;
